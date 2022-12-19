b651cc1dc6c85c7de132353bb9b7522fb8b68712
Mirko Franco <francomirko91@gmail.com>
2022-12-19 23:18:08 +0100

    ---------------

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,9 @@ fun main() {
     val number2 = 4323
     val number3 = 4323
 
-    println("number1 : $number1 , number2: $number2, number3: $number3")
+    println("number1 : $number1")
+    println("number2 : $number2")
+    println("number3 : $number3")
 
 
 }
